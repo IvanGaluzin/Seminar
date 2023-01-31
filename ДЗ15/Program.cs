44d2c08c085887e1d8e7137c0cffec78b042fe37
Ivan Galuzin <galuzin627@gmail.com>
2023-01-31 20:19:15 +0700

    Seminar 2

---
+++ ДЗ15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет, является ли
+// этот день выходным.
+
+Console.Clear();
+
+   Console.Write("Введите номер дня недели: ");
+   int num = int.Parse(Console.ReadLine()!);
+   if (num == 1 || num == 2 ||num == 3 || num == 4 || num == 5)
+   {
+       Console.WriteLine("нет");
+   }
+   else if (num == 7 || num == 6)
+   {
+       Console.WriteLine("да");
+   }
+   else
+   {
+       Console.WriteLine("Введите другое число");
+   }
---
+++ Задача14/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №14. Общее обсуждение
+// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+int b = 7;
+int c = 23;
+int res1 = a % b;
+int res2 = a % c;
+if ( res1 ==0 && res2 == 0)
+{
+Console.Write("Кратно ");
+}
+else
+{
+Console.Write("Не кратно ");
+}
