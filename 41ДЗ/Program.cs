78fc0235ac8966ebf2d046782b8b72cda2cdbb4a
Ivan Galuzin <galuzin627@gmail.com>
2023-02-19 23:56:27 +0700

    Семинар6

---
+++ 41ДЗ/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetPozitive(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите размер масива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(size, -99, 99);
+Console.WriteLine(String.Join(", ", array));
+if (GetPozitive(array) == 0)
+{
+    Console.Write($"Чисел больше 0 : Нет ");
+}
+else
+{
+    Console.Write($"Чисел больше 0 : {GetPozitive(array)}");
+}
---
+++ ЗСЗ_Семинар6/Program.cs
@@ -0,0 +1,38 @@
+﻿int a = 2;
+int b = 0;
+int c = 0;
+int[][] arr = new int[3][];
+
+arr[0] = new int[3] { 1, 2, 3 };
+arr[1] = new int[3] { 4, 5, 6 };
+arr[2] = new int[3] { 7, 8, 9 };
+
+Console.WriteLine($" {arr[0][0]}, {arr[0][1]}, {arr[0][2]}");
+Console.WriteLine($" {arr[1][0]}, {arr[1][1]}, {arr[1][2]}");
+Console.WriteLine($" {arr[2][0]}, {arr[2][1]}, {arr[2][2]}");
+Console.Write("Начиная с 7 против часовой стрелки: ");
+while (c < 9)
+{
+    Console.Write($" {arr[a][b]}");
+    if (b < 2 && c < 2)
+    {
+        b++;
+    }
+    if (a > -1 && c > 1 && c < 4)
+    {
+        a--;
+    }
+    if (b > 0 && c > 3 && c < 6)
+    {
+        b--;
+    }
+    if (a < 1 && c > 5 && c < 8)
+    {
+        a++;
+    }
+    if (b < 2 && c > 6)
+    {
+        b++;
+    }
+    c++;
+}
