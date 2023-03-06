aca500dc1397e551466ecb1fa0aa9fd5611ee078
Ivan Galuzin <galuzin627@gmail.com>
2023-03-06 14:20:24 +0700

    Seminar 9

---
+++ 66ДЗ/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NumberSum(int m, int n)
+{
+    int res = m;
+    if (m == n) return m;
+    m++; res = res + NumberSum(m, n);
+    return res;
+}
+Console.Write("Введите число M = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N = ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма натуральных чисел: {NumberSum(m, n)}");
